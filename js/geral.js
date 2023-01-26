008e7e7040f7d9fb73667364345c6d7e0fe7ca7d
jorgeElegal <jorgetadeujac@gmail.com>
2023-01-25 21:52:31 -0300

    setimo commit

---
+++ js/geral.js
@@ -0,0 +1,19 @@
+
+
+
+function oi() {
+    var janela1 = document.getElementById('janela');
+janela1.style.display = 'block';
+    
+ 
+  }
+
+  function clos() {
+    var janela1 = document.getElementById('janela');
+  
+        janela1.style.display ='none'
+    
+  
+
+ 
+  }
