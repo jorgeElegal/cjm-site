11aae004a1c2703880893b2fa4786d9d07fd96bd
jorgeElegal <jorgetadeujac@gmail.com>
2023-02-12 23:27:42 -0300

    setimo commit

--- js/geral.js
+++ js/geral.js
@@ -17,3 +17,26 @@ janela1.style.display = 'block';
 
  
   }
+
+
+
+  function clos2() {
+    var janela1 = document.getElementById('mode1');
+  
+        janela1.style.display ='none'
+    
+  
+
+ 
+  }
+
+
+  function clos3() {
+    var janela1 = document.getElementById('mode2');
+  
+        janela1.style.display ='none'
+    
+  
+
+ 
+  }
